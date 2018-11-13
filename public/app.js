1a8e815e7b1072dde8398d4c989ee64ca6ca0de1
Alex Monteiro <alexmonteirobarboza@gmail.com>
2018-11-13 14:47:41 -0200

    adicionado lista de grupos

--- public/app.js
+++ public/app.js
@@ -4,12 +4,16 @@
     .config(function($routeProvider, $locationProvider) {
             $routeProvider
             .when('/', {
-                templateUrl: 'principal.html',
+                templateUrl: '/partials/principal.html',
                 controller: 'PrincipalController as vm'
             })
             .when('/novo-grupo', {
-                templateUrl: 'novo-grupo.html',
+                templateUrl: '/partials/novo-grupo.html',
                 controller: 'NovoGrupoController as vm'
+            })
+            .when('/meus-grupos', {
+                templateUrl: '/partials/meus-grupos.html',
+                controller: 'MeusGruposController as vm'
             })
             .otherwise({
                 redirectTo: "/"
@@ -19,15 +23,15 @@
       // $locationProvider.html5Mode(true);
 
       // Initialize Firebase
-      // var config = {
-      //     apiKey: "AIzaSyAwX3AT6ssQzenXbs-dlPkijGgEZJGG0Hw",
-      //     authDomain: "cowclub-39d07.firebaseapp.com",
-      //     databaseURL: "https://cowclub-39d07.firebaseio.com",
-      //     projectId: "cowclub-39d07",
-      //     storageBucket: "cowclub-39d07.appspot.com",
-      //     messagingSenderId: "275804211929"
-      // };
-      // firebase.initializeApp(config);
+      var config = {
+          apiKey: "AIzaSyAwX3AT6ssQzenXbs-dlPkijGgEZJGG0Hw",
+          authDomain: "cowclub-39d07.firebaseapp.com",
+          databaseURL: "https://cowclub-39d07.firebaseio.com",
+          projectId: "cowclub-39d07",
+          storageBucket: "cowclub-39d07.appspot.com",
+          messagingSenderId: "275804211929"
+      };
+      firebase.initializeApp(config);
     })
     .controller('PrincipalController', ['$scope', function PrincipalController($scope) {
         var vm = this;
@@ -38,21 +42,63 @@
 
         vm.init();
     }])
-    .controller('NovoGrupoController', ['$scope', function NovoGrupoController($scope) {
+    .controller('NovoGrupoController', ['$scope', '$location', function NovoGrupoController($scope, $location) {
         var vm = this;
+        vm.dados = null
 
         vm.init = function () {
-            
+            vm.dados = {
+                nome: '',
+                valor: 0,
+                participantes: []
+            };
         };
 
-        function savarUsuario(nome, agencia, conta) {
-            firebase.database().ref('users/' + nome).set({
-              username: nome,
-              agencia: agencia,
-              conta : conta
-            });
+        vm.salvar = function() {
+            savarGrupo(vm.dados);
+        }
+
+        function savarGrupo(dados) {
+            firebase.database().ref('grupos/' + dados.nome).set({
+                valor: dados.valor,
+                participantes: dados.participantes
+            }, function(error) {
+                if (error) {
+                  console.log(error);
+                } else {
+                  // Data saved successfully!
+                  $location.path('/meus-grupos');
+                }
+              });
         }
         
+        vm.init();
+    }])
+    .controller('MeusGruposController', ['$scope', '$timeout', function MeusGruposController($scope, $timeout) {
+        var vm = this;
+        vm.listaGrupos = [];
+
+        vm.init = function () {
+            buscaGrupos();
+        };
+
+        function buscaGrupos(){
+            var gruposRef = firebase.database().ref('grupos');
+            gruposRef.on('value', function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                    $timeout(function(){
+                        vm.listaGrupos.push(
+                            {
+                                nome: childSnapshot.key,
+                                valor: childSnapshot.val().valor,
+                                participantes: []
+                            }
+                        );
+                    });
+                });
+            });
+        }
+
         vm.init();
     }]);
   })(window.angular);
