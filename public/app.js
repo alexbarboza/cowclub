82445ce58bcb0bd506cc014e74036e66a71264c8
Alex Monteiro <alexmonteirobarboza@gmail.com>
2018-11-13 10:08:19 -0200

    com firebase

--- public/app.js
+++ public/app.js
@@ -2,15 +2,32 @@
     'use strict';
   angular.module('CowClub', ['ngRoute'])
     .controller('CowClubController', ['$scope', function CowClubController($scope) {
-      var vm = this;
+        var vm = this;
 
-      vm.tela = '';
-  
-      vm.init = function () {
-        vm.tela = 'tela-login';
-      };
-      
-      vm.init();
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyAwX3AT6ssQzenXbs-dlPkijGgEZJGG0Hw",
+            authDomain: "cowclub-39d07.firebaseapp.com",
+            databaseURL: "https://cowclub-39d07.firebaseio.com",
+            projectId: "cowclub-39d07",
+            storageBucket: "cowclub-39d07.appspot.com",
+            messagingSenderId: "275804211929"
+        };
+        firebase.initializeApp(config);
+
+        vm.init = function () {
+            
+        };
+
+        function savarUsuario(nome, agencia, conta) {
+            firebase.database().ref('users/' + nome).set({
+              username: nome,
+              agencia: agencia,
+              conta : conta
+            });
+        }
+        
+        vm.init();
     }]);
   })(window.angular);
   
